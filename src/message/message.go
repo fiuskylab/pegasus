983e3b8861d77b17e1eb9cd4d252e627d3d79b1c
Rafael <rafael_breno@outlook.com>
2022-09-10 22:43:19 -0300

    feat: Add TopicName field into Message struct

--- src/message/message.go
+++ src/message/message.go
@@ -4,7 +4,8 @@ type (
 	// Message - A data model that represents the messages
 	// sent/received to/from services subscribed to a Topic.
 	Message struct {
-		Body string `json:"body"`
+		Body      string `json:"body"`
+		TopicName string `json:"topic_name"`
 		// TODO:
 		// 	- Add more features, like:
 		// 		- Attributes
