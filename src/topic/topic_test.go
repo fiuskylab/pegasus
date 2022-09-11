06700e0e7dec43ff4079116977f7b0729b2a7a9b
Rafael <rafael_breno@outlook.com>
2022-09-10 23:23:37 -0300

    fix: UUID regex

--- src/topic/topic_test.go
+++ src/topic/topic_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	uuidRegex = regexp.MustCompile("^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89a][0-9a-f]{3}-[0-9a-f]{12}$")
+	uuidRegex = regexp.MustCompile("^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$")
 )
 
 func TestTopic(t *testing.T) {
