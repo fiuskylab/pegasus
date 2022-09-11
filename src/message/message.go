98a1cf773110547759d43228304eff90490b0e49
Rafael <rafael_breno@outlook.com>
2022-09-10 22:43:28 -0300

    feat: Add Message Validate method

--- src/message/message.go
+++ src/message/message.go
@@ -1,5 +1,7 @@
 package message
 
+import "fmt"
+
 type (
 	// Message - A data model that represents the messages
 	// sent/received to/from services subscribed to a Topic.
@@ -13,3 +15,18 @@ type (
 		// 		- ID ???
 	}
 )
+
+const (
+	errEmptyField = "field '%s' is empty"
+)
+
+// Validate - validates Message fields
+func (m *Message) Validate() error {
+	if m.Body == "" {
+		return fmt.Errorf(errEmptyField, "body")
+	}
+	if m.TopicName == "" {
+		return fmt.Errorf(errEmptyField, "topic_name")
+	}
+	return nil
+}
